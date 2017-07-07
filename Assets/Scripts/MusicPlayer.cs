39deb1c8245113cf9f77cafa83f788a317489cd4
Supermaniac101 <Timschijvenaars@hotmail.com>
2017-07-07 02:35:22 +0200

    Init

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public static MusicPlayer mp = null;
+
+    void Awake()
+    {
+        if (mp != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            mp = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+}
